422de472c0ce8e9e8a49afa3c8a66dcadfa172a8
Nathan Soares Silva <nathan.fnss@gmail.com>
2021-05-21 22:14:23 -0300

    enviando...

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { NgSelectModule } from '@ng-select/ng-select';
+import {MatCardModule} from '@angular/material/card';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NgbModule,
+    MatSliderModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    NgSelectModule,
+    MatCardModule
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
